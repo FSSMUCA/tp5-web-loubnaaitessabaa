4d2dfc6994ad0149126b44e0f76485600f33cfd0
loubnaaitessabaa <l.aitessabaa9640@uca.ac.ma>
2025-12-06 09:46:38 +0100

    addscript

---
+++ .github/EXERCICE10.js
@@ -0,0 +1,9 @@
+function sommeIterative(n){
+    let s=0;
+    for(let i=1;i<=n;i++) s+=i;
+    return s;
+}
+function sommeRecursive(n){
+    if(n===1) return 1;
+    return n + sommeRecursive(n-1);
+}
